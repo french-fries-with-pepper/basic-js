f90c234b9f8160097bc7ce8878a7bcdfd7c946e5
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2021-02-28 23:41:22 +0300

    feat: add recursive implementation of calculateDepth function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+  calculateDepth(array) {
+      let level = array.filter((el)=>Array.isArray(el));
+      if(level.length===0)return 1;
+      let next =[];
+      for(let i = 0; i <level.length; i++){
+        next.push(...level[i]);
+      }
+      return 1+this.calculateDepth(next);
+    }
 };
