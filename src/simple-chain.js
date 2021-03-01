fe3d3ab4d9a37581f9c765582d2ab76e26e79a9b
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2021-03-01 11:43:14 +0300

    feat: add implementation of chainMaker object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position - 1 >= 0 && position - 1 < this.arr.length) {
+      this.arr.splice(position - 1, 1);
+      return this;
+    } else {
+      this.arr = [];
+      throw new Error();
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let res = "";
+    for (let i = 0; i < this.arr.length; i++) 
+      res += `~( ${this.arr[i]} )~`;
+    res = res.slice(0, -1);
+    res = res.substring(1);
+    this.arr = [];
+    return res;
+  },
 };
 
 module.exports = chainMaker;
