1e0955ee7a38a5162379721f567e3e9f13324bde
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2021-03-01 14:22:06 +0300

    feat: add ugly, but working implementation of repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let res = [];
+  let addRes = [];
+  const nOfAdds = options.hasOwnProperty("additionRepeatTimes")&&(typeof(options.additionRepeatTimes)==="number")
+    ? options.additionRepeatTimes
+    : 1;
+  for (let i = 0; i < nOfAdds; i++){
+    if(options.addition===null){
+      addRes.push("null");
+      continue;
+    }
+    addRes.push(options.addition);
+  }
   
+  const as = options.hasOwnProperty("additionSeparator")
+    ? options.additionSeparator
+    : "|";
+  const suffix = addRes.join(as);
+  const part = str+suffix;
+
+  const rt = options.hasOwnProperty("repeatTimes")&&(typeof(options.repeatTimes)==="number")
+    ? options.repeatTimes
+    : 1;
+  
+  for (let i = 0; i < rt;i++)
+    res.push(part);
+  const sep = options.hasOwnProperty("separator")
+    ? options.separator
+    : "+";
+  return res.join(sep);
+};
