218912e11fa4fcae812791ba02f0bcd2e38865be
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2021-03-01 21:40:55 +0300

    feat: add implementation of VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  isDirect = true;
+  AL = 26;
+  constructor(isDirect = true){
+    this.isDirect = isDirect;
+  }
+  encrypt(message,key) {
+    if(!message||!key)
+      throw new Error();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let keyString = "";
+    for(let i = 0; i <message.length; i++)
+      keyString+=key[i%key.length];
+    let res = "";
+    for (let i = 0, count = 0; i <message.length; i++){
+      const elCode = message[i].charCodeAt(0);
+      if(elCode<65||elCode>90){
+        res += message[i];
+        continue;
+      } 
+      const keyCode = keyString[count].charCodeAt(0);
+      count++;
+      res+=String.fromCharCode(((keyCode+elCode)%this.AL)+65)
+    }
+    return this.isDirect?res:res.split("").reverse().join("");
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message,key) {
+    key = key.toUpperCase();
+    let keyString = "";
+    for(let i = 0; i <message.length; i++)
+      keyString+=key[i%key.length];
+    let res = "";
+    for (let i = 0, count = 0; i <message.length; i++){
+      const elCode = message[i].charCodeAt(0);
+      if(elCode<65||elCode>90){
+        res += message[i];
+        continue;
+      } 
+      const keyCode = keyString[count].charCodeAt(0);
+      count++;
+      res+=String.fromCharCode(((elCode+this.AL-keyCode)%this.AL)+65)
+    }
+    return this.isDirect?res:res.split("").reverse().join("");
   }
 }
 
