b346cf67e305d7b10476e0ae8b2c6034209eb7a2
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2021-03-04 12:44:30 +0300

    feat: add implementation of dataSample function from carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,10 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(activity) {
+  if(typeof activity !== "string") return false;
+  const n = parseFloat(activity);
+  if(isNaN(n)||n<=0||n>15) return false;
+  const result = Math.ceil((Math.log(MODERN_ACTIVITY/n))/(0.693/HALF_LIFE_PERIOD));
+  return result;
 };
